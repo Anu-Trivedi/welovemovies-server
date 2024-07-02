6a1790ede631c632603eb809efe1c047200557ed
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2024-07-01 22:35:57 -0700

    modified movies.service

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -4,11 +4,11 @@ function list(isShowing = false) {
   if (isShowing) {
     return knex("movies as m")
       .join("movies_theaters as mt", "mt.movie_id", "m.movie_id")
-      .select("*")
+      .select("m.movie_id", "m.title", "m.runtime_in_minutes", "m.rating", "m.description", "m.image_url")
       .where({ "mt.is_showing": true })
-      .groupBy("mt.movie_id, m.movie_id");
+      .groupBy("m.movie_id");
   }
-  return knex("movies as m").select("*");
+  return knex("movies");
 }
 
 function read(movie_id) {
